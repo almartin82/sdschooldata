20b4500813b93863ad164f239d80d96b2739ee85
almartin82 <almartin@gmail.com>
2026-01-03 19:18:49 -0500

    Fix R CMD check: vignette eval=FALSE, remove unused imports, add globalVariables

--- R/sdschooldata-package.R
+++ R/sdschooldata-package.R
@@ -4,3 +4,8 @@
 ## usethis namespace: start
 ## usethis namespace: end
 NULL
+
+# Global variables used in dplyr pipelines
+utils::globalVariables(c(
+  "district_type_code", "grade_level", "n_students", "row_total", "subgroup", "type"
+))
